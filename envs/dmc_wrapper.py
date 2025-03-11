7fe095b4e09cc9b6baf220bbc9e077ba3ed6a16a
Shubham Subhnil <shubhamsubhniloptimus@gmail.com>
2025-03-11 14:26:09 +0000

    Refractors for DMC compatibility

---
+++ envs/dmc_wrapper.py
@@ -0,0 +1,82 @@
+# In env_wrapper.py
+import gymnasium as gym
+import numpy as np
+from dm_control import suite
+from shimmy.dm_control_compatibility import DmControlCompatibilityV0
+
+
+class DMControl(gym.Env):
+    def __init__(
+            self,
+            name,
+            action_repeat=4,
+            size=(64, 64),
+            camera=None,
+            seed=None,
+    ):
+        # Parse domain and task from name (e.g., "dm_control/cartpole-balance")
+        domain_name, task_name = name.split('/')[-1].split('-')
+
+        # Create DM Control environment
+        self._env = suite.load(domain_name, task_name, task_kwargs={'random': seed})
+
+        # Wrap with compatibility class
+        self._env = DmControlCompatibilityV0(
+            self._env,
+            render_mode="rgb_array",
+            render_height=size[0],
+            render_width=size[1],
+            camera_id=camera if camera is not None else 0
+        )
+
+        self._action_repeat = action_repeat
+        self._size = size
+        self._seed = seed
+
+        # Set action and observation spaces
+        self.action_space = self._env.action_space
+        self.observation_space = gym.spaces.Box(
+            low=0, high=255,
+            shape=(size[0], size[1], 3),
+            dtype=np.uint8
+        )
+
+    def step(self, action):
+        total_reward = 0.0
+        for _ in range(self._action_repeat):
+            _, reward, terminated, truncated, info = self._env.step(action)
+
+            # Get image observation instead
+            image = self._env._env.physics.render(
+                height=self._size[0],
+                width=self._size[1],
+                camera_id=self._env.camera_id
+            )
+
+            total_reward += reward
+            if terminated or truncated:
+                break
+
+        # Return in format expected by your code (4 values)
+        is_last = terminated or truncated
+        info['is_terminal'] = terminated  # Store termination state in info
+        image = np.ascontiguousarray(image)
+
+        return image, total_reward, is_last, info
+
+    def reset(self):
+        # Original reset returns state-based observation
+        _, info = self._env.reset(seed=self._seed)
+
+        # Get image observation instead
+        image = self._env._env.physics.render(
+            height=self._size[0],
+            width=self._size[1],
+            camera_id=self._env.camera_id
+        )
+        image = np.ascontiguousarray(image)
+
+        return image, info
+
+    def close(self):
+        return self._env.close()
